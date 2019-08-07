1a4c0256cb12b46ad2ab29a9054cb7489eca866e
andresslacson1989 <48279587+andresslacson1989@users.noreply.github.com>
2019-08-07 12:30:54 +0800

    Update rclocal2.sh

--- rclocal2.sh
+++ rclocal2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm /etc/rc.local
 cd 
+chmod +x /etc/openvpn/disconnect.sh
+chmod +x /etc/openvpn/connect.sh
 wget https://raw.githubusercontent.com/andresslacson1989/Debian9/master/rc.local2
 chmod +x rc.local2
 mv rc.local2 /etc/rc.local
